da7dc3fc7cb3765ae60cb78cc48215450f2fae61
James Stubblefield <jameswilliamiii@gmail.com>
2014-04-21 22:33:45 -0500

    add image if no profile photo present, add abide content to form, complete profiles controller, add partial for alerts and messages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,11 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile_instance_var, except: [ :index, :new, :create ]
+
   def index
   end
 
   def show
+
   end
 
   def new
@@ -10,14 +13,31 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    @profile = Profile.new profile_params
+    if @profile.save
+      redirect_to @profile, notice: "Profile successfully created"
+    else
+      render 'new'
+    end
   end
 
   def edit
+
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :company, :public, :details, :twitter, :facebook, :linkedin, :github, :website, :email)
+  end
+
+  def set_profile_instance_var
+    @profile = Profile.find params[:id]
   end
 end
