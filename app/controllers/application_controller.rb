cb7558a229e265f7e24f74a23f45f87450feb325
James Stubblefield <jameswilliamiii@gmail.com>
2014-04-22 17:33:09 -0500

    adjust params call to be wrapped as an array

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :membership_type, :first_name, :last_name
-    devise_parameter_sanitizer.for(:account_update) << :membership_type, :first_name, :last_name
-    devise_parameter_sanitizer.for(:accept_invitation) << :membership_type, :first_name, :last_name
+    devise_parameter_sanitizer.for(:sign_up) << [:membership_type, :first_name, :last_name]
+    devise_parameter_sanitizer.for(:account_update) << [:membership_type, :first_name, :last_name]
+    devise_parameter_sanitizer.for(:accept_invitation) << [:membership_type, :first_name, :last_name]
   end
 end
