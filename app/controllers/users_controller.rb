c557e82203c9f5d46eeee7778f4f0349fbcbf44b
James Stubblefield <jameswilliamiii@gmail.com>
2014-05-02 09:09:16 -0500

    add before_action to authenticate_user! in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action
+  before_action :authenticate_user!
+
   def show
     @user = current_user
     @profile = @user.profile if @user.has_profile?
