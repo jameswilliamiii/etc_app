993751f51c8573274824bb294dd5440eaffb5a29
James Stubblefield <jameswilliamiii@gmail.com>
2014-05-14 15:53:07 -0500

    add mailchimp integration to application

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,10 +14,23 @@ class StaticPagesController < ApplicationController
 
   def apply_email
     form = params[:application]
+    add_to_email_list(params[:application]['email'])
     ContactMailer.membership_application(form).deliver
     redirect_to apply_confirmation_url, notice: "Your application was successfully submitted"
   end
 
   def apply_confirmation
+  end
+
+  private
+
+  def add_to_email_list(email)
+    Mailchimp::API.new(ENV['MC_API_KEY']).list_subscribe({
+                                                            :id => ENV["MC_CURRENT_USER_LIST_ID"],
+                                                            :email_address => email,
+                                                            :merge_vars => {},
+                                                            :double_optin => false,
+                                                            :update_existing => true
+                                                          })
   end
 end
