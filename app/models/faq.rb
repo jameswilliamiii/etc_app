c0e890b647135eb865ba4a0a45c3ee7ea483a14b
James Stubblefield <jameswilliamiii@gmail.com>
2014-05-27 10:36:28 -0500

    add more pending test specs for profile. Remove faq model since it is no longer used.

--- app/models/faq.rb
+++
@@ -1,4 +0,0 @@
-class Faq < ActiveRecord::Base
-  belongs_to :class_offer
-  validates_presence_of [ :question, :answer ]
-end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Profile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory"
+  it "should be invalid without a name"
+  it "should be invalid without a profile_type"
+  it "should be invalid without a  user_id"
+  it { should belong_to(:user) }
+  context "return by membership_type & member_since" do
+    it "should should return items ordered by membership_type"
+    it "should should return items ordered by member_since in ASC order"
+  end
+  it "should should return items ordered by membership_type"
+  it "should filter results by a skill"
+  it "should filter results by membership_type"
+  it "should be be invalid if profile is not public"
+  it "should only show profiles which are public"
 end
