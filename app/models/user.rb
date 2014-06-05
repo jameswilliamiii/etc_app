9ad09b16524e0a452259e3b2f2d1640542bc70dd
James Stubblefield <jameswilliamiii@gmail.com>
2014-06-05 12:25:00 -0500

    make remove_from_members_group a before_destroy callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :profiles
 
   after_create :add_to_mailchimp_list if Rails.env.production?
-  after_destroy :remove_from_members_group if Rails.env.production?
+  before_destroy :remove_from_members_group if Rails.env.production?
 
   validates_presence_of [ :email, :first_name, :last_name, :membership_type, :member_since ]
 
