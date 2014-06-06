02c39abce3dfb7b17dcda15ddffeabd2609caeef
James Stubblefield <jameswilliamiii@gmail.com>
2014-06-06 16:56:31 -0500

    redesign class_offers#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,9 @@ module ApplicationHelper
 
   def class_schedule(class_offer)
     if class_offer.start_date == class_offer.end_date
-      h "<li> #{@class_offer.start_date.strftime('%D')}, #{@class_offer.time} </li>".html_safe
+      h "<li> #{class_offer.start_date.strftime('%D')}, #{class_offer.time} </li>".html_safe
     else
-      h "<li> #{@class_offer.start_date.strftime('%D')} through #{@class_offer.end_date.strftime('%D')}, #{@class_offer.time} </li>".html_safe
+      h "<li> #{class_offer.start_date.strftime('%D')} through #{class_offer.end_date.strftime('%D')}, #{class_offer.time} </li>".html_safe
     end
   end
 end
