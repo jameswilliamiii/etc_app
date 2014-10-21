1104ab43d6969731466b002e43434802ccb3ad4a
James Stubblefield <jameswilliamiii@gmail.com>
2014-10-20 21:21:37 -0500

    added address to the membership application

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,12 +1,70 @@
 module StaticPagesHelper
+
   def is_premier?(params)
     unless params[:membership_type].present? && params[:membership_type] == "standard"
       "checked"
     end
   end
+
   def is_standard?(params)
     if params[:membership_type].present? && params[:membership_type] == "standard"
       "checked"
     end
+  end
+
+  def us_states
+    [
+      ['AK', 'AK'],
+      ['AL', 'AL'],
+      ['AR', 'AR'],
+      ['AZ', 'AZ'],
+      ['CA', 'CA'],
+      ['CO', 'CO'],
+      ['CT', 'CT'],
+      ['DC', 'DC'],
+      ['DE', 'DE'],
+      ['FL', 'FL'],
+      ['GA', 'GA'],
+      ['HI', 'HI'],
+      ['IA', 'IA'],
+      ['ID', 'ID'],
+      ['IL', 'IL'],
+      ['IN', 'IN'],
+      ['KS', 'KS'],
+      ['KY', 'KY'],
+      ['LA', 'LA'],
+      ['MA', 'MA'],
+      ['MD', 'MD'],
+      ['ME', 'ME'],
+      ['MI', 'MI'],
+      ['MN', 'MN'],
+      ['MO', 'MO'],
+      ['MS', 'MS'],
+      ['MT', 'MT'],
+      ['NC', 'NC'],
+      ['ND', 'ND'],
+      ['NE', 'NE'],
+      ['NH', 'NH'],
+      ['NJ', 'NJ'],
+      ['NM', 'NM'],
+      ['NV', 'NV'],
+      ['NY', 'NY'],
+      ['OH', 'OH'],
+      ['OK', 'OK'],
+      ['OR', 'OR'],
+      ['PA', 'PA'],
+      ['RI', 'RI'],
+      ['SC', 'SC'],
+      ['SD', 'SD'],
+      ['TN', 'TN'],
+      ['TX', 'TX'],
+      ['UT', 'UT'],
+      ['VA', 'VA'],
+      ['VT', 'VT'],
+      ['WA', 'WA'],
+      ['WI', 'WI'],
+      ['WV', 'WV'],
+      ['WY', 'WY']
+    ]
   end
 end
